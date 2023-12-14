dbcd8e4488a975d452f7478f96a9f16c4ea8e237
MSZ <m.szabo@outlook.de>
2023-12-14 23:15:41 +0100

    listModal nimmt jetzt auch title und info

--- app/components/modals/ListModal.tsx
+++ app/components/modals/ListModal.tsx
@@ -23,7 +23,9 @@ const ListModal: React.FC<ListModalProps> = ({ open, onClose }) => {
   const [step, setStep] = useState<STEPS>(STEPS.TASK);
   const [error, setError] = useState("");
   const [task, setTask] = useState<typeof tasks | null>(null);
-  const [location, setLocation] = useState();
+  const [location, setLocation] = useState("");
+  const [info, setInfo] = useState("");
+  const [title, setTitle] = useState("");
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -57,7 +59,9 @@ const ListModal: React.FC<ListModalProps> = ({ open, onClose }) => {
   useEffect(() => {
     console.log("task :>> ", task);
     console.log("location :>> ", location);
-  }, [task, location]);
+    console.log("info :>> ", info);
+    console.log("title :>> ", title);
+  }, [task, location, info, title]);
   let bodyContent = (
     <div className="flex flex-col gap-8 ">
       <h1>Which tasks you want to have done ?</h1>
@@ -78,12 +82,41 @@ const ListModal: React.FC<ListModalProps> = ({ open, onClose }) => {
   if (step === STEPS.LOCATION) {
     bodyContent = (
       <div>
-        <h1>Where is your location?</h1>
+        <form action={onNext}>
+          <h1>Where is your location?</h1>
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            type="text"
+            placeholder="Adress"
+            className="bg-zinc-100 rounded-lg "
+          />
+        </form>
+      </div>
+    );
+  }
+  //   title: String;
+  //   description: String;
+  //   tasks: [String];
+  //   location: String;
+  //   info: String;
+  //   image: [String];
+  //   minGuests: number;
+  //   maxGuests: number;
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div>
+        <h1>Give a short description of your listing</h1>
         <input
-          onChange={(e) => setLocation(e.target.value)}
+          onChange={(e) => setTitle(e.target.value)}
           type="text"
-          placeholder="Adress"
-          className="bg-zinc-100 rounded-lg"
+          placeholder="Title"
+          className="bg-zinc-100 rounded-lg mt-2 w-full"
+        />
+        <input
+          onChange={(e) => setInfo(e.target.value)}
+          type="text"
+          placeholder="Info"
+          className="bg-zinc-100 rounded-lg py-5 mt-2 w-full"
         />
       </div>
     );
