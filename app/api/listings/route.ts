f56cbdf962e58ea441ff3dd5ce4bf1ef0f439175
MSZ <m.szabo@outlook.de>
2023-12-19 11:18:53 +0100

    Listings userID wird nun als ObjectID angenommen

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -52,6 +52,7 @@ import connectMongo from "@/app/config/mongo";
 import Listing from "@/app/schemas/listing";
 import { NextResponse } from "next/server";
 import { getSession } from "next-auth/react";
+import mongoose from "mongoose";
 
 export async function POST(request: Request) {
   const formData = await request.formData();
@@ -66,8 +67,8 @@ export async function POST(request: Request) {
   const images = formData.getAll("images");
   const minGuests = formData.get("minGuests");
   const maxGuests = formData.get("maxGuests");
-  const user = formData.get("user");
-  console.log("id :>> ", user);
+  const data = formData.get("user");
+  const user = new mongoose.Types.ObjectId(data?.toString());
 
   try {
     await connectMongo();
